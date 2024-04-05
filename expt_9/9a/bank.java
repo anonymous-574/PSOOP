57259cadc9ee59f48b65eb60d3a841bb1f3a0c07
anonymous-574 <140256100+anonymous-574@users.noreply.github.com>
2024-04-05 13:41:23 +0000

    Add files via upload

---
+++ expt_9/9a/bank.java
@@ -0,0 +1,170 @@
+import java.util.*;
+
+class balance_error extends Exception
+{
+    @Override
+    public String toString() {
+        return"User has balance less than 0 or 0";
+    }
+}
+
+class min_bal_err extends Exception
+{
+    @Override
+    public String toString() {
+       return"User has less money than min balance allowed of 500rs";
+    }
+}
+
+class withdrawal_fail1 extends Exception
+{
+    @Override
+    public String toString() {
+        return "Withdrawal failed because you do not have enough money";
+    }
+}
+
+class withdrawal_fail2 extends Exception
+{
+    @Override
+    public String toString() {
+        return "You cannot withdraw 0 or negative money";
+    }
+}
+
+class deposit_fail extends Exception
+{
+    @Override
+    public String toString() {
+        return "You cannot deposit negative money";
+    }
+}
+
+
+
+
+abstract class account
+{
+    String name;
+    double balance,interest , min_bal=500;
+    long acc_no;
+    abstract void deposit(double d);
+    abstract void withdraw(double w);
+    abstract void print();
+    abstract void add_interest();
+}
+
+class saving_acc extends account
+{
+
+    saving_acc(String n , double b , long acc_no, double i)
+    {
+        this.name=n;
+        this.acc_no=acc_no;
+        this.interest=i;
+
+        try
+        {
+        this.balance=b;
+        if (balance<=0) 
+        {
+         throw new balance_error();    
+        }
+
+        if (balance<min_bal) 
+        {
+            throw new min_bal_err();
+        }
+
+        }
+        catch (Exception e) {
+           System.out.println(e);
+           System.exit(0);
+        }
+
+    }
+
+    @Override
+    void deposit(double d) 
+    {
+        try {
+            if (d<0) {
+                throw new deposit_fail();
+            }
+            balance+=d;
+        } catch (Exception e) {
+            System.out.println(e);
+            System.exit(0);
+        }
+    }
+
+    @Override
+    void withdraw(double w) {
+        
+        try {
+            
+            if (balance<w) {
+                throw new withdrawal_fail1();
+            }
+            if (balance<=0) {
+                throw new withdrawal_fail2();
+            }
+            balance-=w;
+        } catch (Exception e) {
+            System.out.println(e);
+            System.exit(0);
+        }
+    }
+
+    @Override
+    void print() {
+        System.out.println("User: "+name+" of account number: "+acc_no+" has balance "+balance);
+    }
+
+    @Override
+    void add_interest() throws ArithmeticException {
+        if (interest<=0) 
+        {
+         throw new ArithmeticException();    
+        }
+        balance= balance*interest*1;
+    }
+
+}
+
+
+public class bank {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("what is your name?");
+        String nam = sc.next();
+        System.out.println("what is your current account balance? ");
+        double cur_bal =sc.nextDouble();
+
+
+        System.out.println("what is interest rate? ");
+        double interest = sc.nextDouble();
+        System.out.println("What is account number? ");
+        long acc_no =sc.nextLong();
+        saving_acc s = new saving_acc(nam, cur_bal, acc_no, interest);
+
+        try {
+            s.add_interest();
+        } catch (Exception e) {
+            System.out.println(e);
+            System.exit(0);
+        }
+
+        System.out.println("How much do you want to deposit? ");
+        double depo = sc.nextDouble();
+        s.deposit(depo);
+        System.out.println("how much do you want to withdraw? ");
+        double with = sc.nextDouble();
+        s.withdraw(with);
+
+        s.print();
+
+        sc.close();
+    }
+}
